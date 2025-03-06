e85d4f1135910c71af6161f71f11155db201993b
andrjre <andrjre@gmail.com>
2025-03-06 07:39:14 +0000

    Commit Commit

---
+++ index.js
@@ -0,0 +1,67 @@
+
+
+let rock = 0;
+let paper = 1;
+let scissors = 0;
+let computerChoice = document.getElementById("computerChoice");
+let playerChoice = document.getElementById("playerChoice");
+let button = document.getElementsByClassName("button");
+let rockBtn = document.getElementById("rockBtn");
+let paperBtn = document.getElementById("paperBtn");
+let scissorsBtn = document.getElementById("scissorsBtn");
+let computer;
+let result = document.getElementById("result");
+
+
+rockBtn.onclick = function(){
+    playerChoice.innerHTML = "You chose rock"
+    computer = Math.floor(Math.random() * 3)
+        if(computer == 0){
+            computerChoice.innerHTML = "Computer chose rock";
+            result.innerHTML = "DRAW!"
+        }
+        else if(computer == 1){
+            computerChoice.innerHTML = "Computer chose paper";
+            result.innerHTML = "YOU LOSE..."
+        }
+        else if (computer == 2){
+            computerChoice.innerHTML = "Computer chose scissors";
+            result.innerHTML = "YOU WIN!"
+        }
+}
+
+paperBtn.onclick = function(){
+    playerChoice.innerHTML = "You chose paper"
+    computer = Math.floor(Math.random() * 3)
+        if(computer == 0){
+            computerChoice.innerHTML = "Computer chose rock";
+            result.innerHTML = "YOU WIN!"
+        }
+        else if(computer == 1){
+            computerChoice.innerHTML = "Computer chose paper";
+            result.innerHTML = "DRAW!"
+        }
+        else if (computer == 2){
+            computerChoice.innerHTML = "Computer chose scissors";
+            result.innerHTML = "YOU LOSE..."
+        }
+}
+
+scissorsBtn.onclick = function(){
+    playerChoice.innerHTML = "You chose scissors"
+    computer = Math.floor(Math.random() * 3)
+        if(computer == 0){
+            computerChoice.innerHTML = "Computer chose rock";
+            result.innerHTML = "YOU LOSE..."
+        }
+        else if(computer == 1){
+            computerChoice.innerHTML = "Computer chose paper";
+            result.innerHTML = "YOU WIN!"
+        }
+        else if (computer == 2){
+            computerChoice.innerHTML = "Computer chose scissors";
+            result.innerHTML = "DRAW..."
+        }
+}
+
+
