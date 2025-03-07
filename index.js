f89b21ff1f72749594c5951fefa717a67fd1fc0c
andrjre <andrjre@gmail.com>
2025-03-07 19:27:30 +0000

    Made it all work

--- index.js
+++ index.js
@@ -3,31 +3,81 @@
 let rock = 0;
 let paper = 1;
 let scissors = 0;
-let computerChoice = document.getElementById("computerChoice")
-
+let computerChoice = document.getElementById("computerChoice");
+let playerChoice = document.getElementById("playerChoice");
+let button = document.getElementsByClassName("button");
 let rockBtn = document.getElementById("rockBtn");
 let paperBtn = document.getElementById("paperBtn");
 let scissorsBtn = document.getElementById("scissorsBtn");
+let computer;
+let result = document.getElementById("result");
 
-let computer = Math.floor(Math.random() * 3)
+let playerChoiceImg = document.getElementById("playerChoiceImg");
+let computerChoiceImg = document.getElementById("computerChoiceImg")
+
+
+
 
 rockBtn.onclick = function(){
-    console.log("rock")
+    playerChoiceImg.src = "IMAGES/ROCK.png"
+    playerChoice.innerHTML = "You chose rock"
+    computer = Math.floor(Math.random() * 3)
+        if(computer == 0){
+            computerChoice.innerHTML = "Computer chose rock";
+            computerChoiceImg.src = "IMAGES/ROCK.png"
+            result.innerHTML = "DRAW!"
+        }
+        else if(computer == 1){
+            computerChoice.innerHTML = "Computer chose paper";
+            computerChoiceImg.src = "IMAGES/PAPER.png"
+            result.innerHTML = "YOU LOSE..."
+        }
+        else if (computer == 2){
+            computerChoice.innerHTML = "Computer chose scissors";
+            computerChoiceImg.src = "IMAGES/SCISSORS.png"
+            result.innerHTML = "YOU WIN!"
+        }
 }
+
 paperBtn.onclick = function(){
-    console.log("paper")
+    playerChoiceImg.src = "IMAGES/PAPER.png"
+    playerChoice.innerHTML = "You chose paper"
+    computer = Math.floor(Math.random() * 3)
+        if(computer == 0){
+            computerChoice.innerHTML = "Computer chose rock";
+            computerChoiceImg.src = "IMAGES/ROCK.png"
+            result.innerHTML = "YOU WIN!"
+            
+        }
+        else if(computer == 1){
+            computerChoice.innerHTML = "Computer chose paper";
+            computerChoiceImg.src = "IMAGES/PAPER.png"
+            result.innerHTML = "DRAW!"
+        }
+        else if (computer == 2){
+            computerChoice.innerHTML = "Computer chose scissors";
+            computerChoiceImg.src = "IMAGES/SCISSORS.png"
+            result.innerHTML = "YOU LOSE..."
+        }
 }
+
 scissorsBtn.onclick = function(){
-    console.log("scissors")
-}
-
-
-if(computer == 0){
-    computerChoice.innerHTML = "rock";
-}
-else if(computer == 1){
-    computerChoice.innerHTML = "paper";
-}
-else if (computer == 2){
-    computerChoice.innerHTML = "scissors";
+    playerChoiceImg.src = "IMAGES/SCISSORS.png"
+    playerChoice.innerHTML = "You chose scissors"
+    computer = Math.floor(Math.random() * 3)
+        if(computer == 0){
+            computerChoice.innerHTML = "Computer chose rock";
+            computerChoiceImg.src = "IMAGES/ROCK.png"
+            result.innerHTML = "YOU LOSE..."
+        }
+        else if(computer == 1){
+            computerChoice.innerHTML = "Computer chose paper";
+            computerChoiceImg.src = "IMAGES/PAPER.png"
+            result.innerHTML = "YOU WIN!"
+        }
+        else if (computer == 2){
+            computerChoice.innerHTML = "Computer chose scissors";
+            computerChoiceImg.src = "IMAGES/SCISSORS.png"
+            result.innerHTML = "DRAW..."
+        }
 }
